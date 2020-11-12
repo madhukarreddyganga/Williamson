3482a6cf432190848c074e7146dbfb265577ef61
madhukarreddyganga <45326658+madhukarreddyganga@users.noreply.github.com>
2020-11-12 15:07:29 -0500

    Changes to bootstarp

--- src/App.js
+++ src/App.js
@@ -27,7 +27,7 @@ class App extends React.Component {
       });
   }
 
-  images = () => {
+  Images = () => {
     return this.state.images.map((item, index) => {
       return (
         <span key={index}>
@@ -52,19 +52,25 @@ class App extends React.Component {
   };
   heroImage = () => {
     return this.state.heros.map((hero, index) => {
-      const { href, alt, width, rel, meta, size, height } = hero;
+      const { href, alt, width, rel, meta, size, height, images } = hero;
       return (
-        <img
-          onClick={() => heroImages(hero.images)}
-          key={index}
-          src={href}
-          alt={alt}
-          width={width}
-          height={height}
-          rel={rel}
-          meta={meta}
-          size={size}
-        />
+        <div data-ride="carousel">
+          <div class="carousel-inner">
+            <div class="carousel-item active">
+              <img
+                onClick={(heroImages) => heroImages(images)}
+                key={index}
+                src={href}
+                alt={alt}
+                width={width}
+                height={height}
+                rel={rel}
+                meta={meta}
+                size={size}
+              />
+            </div>
+          </div>
+        </div>
       );
     });
   };
@@ -73,55 +79,6 @@ class App extends React.Component {
     return (
       <div className="App">
         <h2>Start editing to see some magic happen!</h2>
-        {/* <div>
-          {this.state.names.map((name, index) => {
-            return <span key={index}>names:{name.name}</span>;
-          })}
-        </div> */}
-        <div>
-          {this.state.names.map((name, index) => {
-            return <span key={index}>names:{name.name}</span>;
-          })}
-          {this.state.images.map((item, index) => {
-            return (
-              <span key={index}>
-                {item.images.map((image, index) => {
-                  const { href, alt, width, rel, meta, size, height } = image;
-                  return (
-                    <img
-                      key={index}
-                      src={href}
-                      alt={alt}
-                      width={width}
-                      height={height}
-                      rel={rel}
-                      meta={meta}
-                      size={size}
-                    />
-                  );
-                })}
-              </span>
-            );
-          })}
-        </div>
-        <div onClick={this.heroImage}>
-          {this.state.heros.map((hero, index) => {
-            return (
-              <span>
-                {/* const { href, alt, width, rel, meta, size, height } = hero; */}
-                <img
-                  src={hero.href}
-                  alt={hero.alt}
-                  width={hero.width}
-                  height={hero.height}
-                  rel={hero.rel}
-                  meta={hero.meta}
-                  size={hero.size}
-                />
-              </span>
-            );
-          })}
-        </div>
       </div>
     );
   }
